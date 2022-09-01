15fcb5033a90adad77577900a461333730ec9c0e
Jade648 <91159734+Jade648@users.noreply.github.com>
2022-09-01 14:06:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Triplet<String> Hours = new Triplet <> ("Hours","Minutes","Seconds");
+      Triplet<String> Hours = new Triplet <> ("Hours","Minutes","Seconds");
+
+        Triplet <String> Icecreamflavor = new Triplet <> ("Strawberry", "Chocolate", "Vanilla");
 
     }
 }
